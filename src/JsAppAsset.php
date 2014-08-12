6e53268e101b6b073b2b0b46a1ddf7e2c3a5d5a5
Petr Grishin <petr.grishin@grishini.ru>
2014-08-12 23:55:45 +0400

    Реализация компонента интеграции фреймворка jsapp

---
+++ src/JsAppAsset.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @author Petr Grishin <petr.grishin@grishini.ru>
+ */
+
+namespace PetrGrishin\Asset\JsApp;
+
+
+use CApplicationComponent;
+use CAssetManager;
+use CClientScript;
+use Yii;
+
+class JsAppAsset extends CApplicationComponent {
+
+    private $vendorPath;
+
+    public function init() {
+        parent::init();
+        $vendorPath = $this->getVendorPath();
+        $assetPath = $this->getAssetManager()->publish($vendorPath . '/petrgrishin/jsapp/build/');
+        $this->getClientScript()->registerScriptFile($assetPath . '/app.js');
+    }
+
+    public function setVendorPath($vendorPath) {
+        $this->vendorPath = $vendorPath;
+        return $this;
+    }
+
+    protected function getVendorPath() {
+        return $this->vendorPath ?: sprintf('%s/../vendor', Yii::getPathOfAlias('application'));
+    }
+
+    /**
+     * @return CAssetManager
+     */
+    protected function getAssetManager() {
+        return $this->getApp()->getComponent('assetManager');
+    }
+
+    /**
+     * @return CClientScript
+     */
+    protected function getClientScript() {
+        return $this->getApp()->getComponent('clientScript');
+    }
+
+    protected function getApp() {
+        return Yii::app();
+    }
+}
+ 
